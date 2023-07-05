13e2a129ca2701fb787e414f9e18c13834f6a2b7
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-05 09:09:52 -0500

    feat: add cors configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import userRouter from './routers/user.router.js'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRouter)
 
-app.listen(8080, () => console.log('Server Up!'))
+app.listen(8081, () => console.log('Server Up!'))
